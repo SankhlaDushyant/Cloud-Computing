599b5d6a226920895e47e4458a0cdc86ddb9a71c
SankhlaDushyant <sankhla.dushyant@gmail.com>
2016-12-26 13:56:02 -0500

    wiki page view algorithm commit 1

---
+++ Wiki Page View Analysis/Readme.txt
@@ -0,0 +1,44 @@
+//Application project group 5
+  Team Members :   Sankhla Dushyant [ds643]	    	   Mehta Bhargav  [bdm9]                   Ranashing Shivanjali V [svr24]
+
+SNAPID use:
+
+1. Select region of was to US East (N.Virginia)
+2. While providing storage in EBS, use the snapid - snap-753dfc1c and add a drive.
+  
+Running the Application: 
+
+1. Create the class files
+javac -classpath $HADOOP_HOME/share/hadoop/common/hadoop-common-2.7.1.jar:$HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.7.1.jar:$HADOOP_HOME/share/hadoop/common/lib/commons-cli-1.2.jar -d ~/Downloads *.java
+
+2. Transfer class files into a folder
+cp ~/Downloads/class1
+
+3. Create the .jar file
+jar -cvf wordcount1.jar -C ~/Downloads/class1 .
+
+4. Make a directory in hdfs 
+hdfs dfs -mkdir /trend
+
+5. Copy the states folder into the folder
+hdfs dfs -copyFromLocal /data1/wikistats/pa* /trend
+- we found faster results by dividing the months in separate folders and processing them.
+
+6. Run the .jar file ( output1 is created automatically )
+hadoop jar ~/Downloads/wordcount1.jar wordcnt /trend /output1 /output2
+
+7. To print output on screen
+ hdfs dfs -cat /output1/part-r-00000  
+ hdfs dfs -cat /output2/part-r-00000    
+
+
+To mount the snap of wiki data:
+
+1. lsblk
+2. sudo mkdir /data1
+3. sudo mount /dev/xvdf1 /data1   (it should be around 170gb)
+
+
+
+
+
---
+++ Wiki Page View Analysis/reducefn.java
@@ -0,0 +1,147 @@
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+
+public class reducefn extends Reducer<Text,Text,Text,Text> {
+
+	private static final int String = 0;
+	Map<String,Integer> outrmp = new HashMap<String,Integer>();
+	Map<String, Integer> pagebucket = new HashMap<String, Integer>();
+	Map<String, String> mnthpg = new HashMap<String, String>();
+	Map<String,BigInteger> pagesizelist = new HashMap<String,BigInteger>();
+	
+	IntWritable result = new IntWritable();
+	static String highPageName=null;
+
+	public void reduce(Text key, Iterable<Text> values, Context context) throws
+		IOException, InterruptedException
+	{
+		
+		int viewCount = 0;
+		BigInteger pageSize = null;
+		
+		for (Text val : values)
+		{
+			String tokens[]=val.toString().trim().split(":");
+			viewCount=Integer.parseInt(tokens[0]);
+			pageSize=new BigInteger(tokens[1]);
+			
+		}
+		result.set(viewCount);
+		pagebucket.put(key.toString(), viewCount);
+		pagesizelist.put(key.toString(), pageSize);
+	}
+
+
+	@Override
+	protected void cleanup(Context context)
+			throws IOException, InterruptedException {
+		outrmp.put("JANUARY",0);
+		outrmp.put("FEBRUARY",0);
+		outrmp.put("MARCH",0);
+		outrmp.put("APRIL",0);
+		outrmp.put("MAY",0);
+		outrmp.put("JUNE",0);
+		outrmp.put("JULY",0);
+		outrmp.put("AUGUST",0);
+		outrmp.put("SEPTEMBER",0);
+		outrmp.put("OCTOBER",0);
+		outrmp.put("NOVEMBER",0);
+		outrmp.put("DECEMBER",0);
+		
+		mnthpg.put("JANUARY",null);
+		mnthpg.put("FEBRUARY",null);
+		mnthpg.put("MARCH",null);
+		mnthpg.put("APRIL",null);
+		mnthpg.put("MAY",null);
+		mnthpg.put("JUNE",null);
+		mnthpg.put("JULY",null);
+		mnthpg.put("AUGUST",null);
+		mnthpg.put("SEPTEMBER",null);
+		mnthpg.put("OCTOBER",null);
+		mnthpg.put("NOVEMBER",null);
+		mnthpg.put("DECEMBER",null);
+		
+		// TODO Auto-generated method stub
+		super.cleanup(context);
+		
+		
+		
+		for(Entry<String, Integer> entry : pagebucket.entrySet())
+		{
+			String keytemp[] = entry.getKey().toString().split(":");
+			
+			String month = keytemp[0];
+			String pageName = keytemp[1];
+			int visitCount = entry.getValue();
+			if(outrmp.get(month)<visitCount)
+			{
+				mnthpg.put(month, pageName);
+				outrmp.put(month, visitCount);
+			}
+			
+		}
+		
+		context.write(new Text("*********************************"),null);
+		
+		context.write(new Text("\t\t\tHIGHEST VIEWCOUNT IN EACH MONTH\t\t\t"),null);
+		context.write(new Text("*********************************"),null);
+		context.write(new Text("MONTH NAME \t\tPAGE NAME\t\tVIEW COUNT\t\t"),null);
+		context.write(new Text("--------------------------------------------"),null);
+		
+		for(Entry<String, Integer> ent : outrmp.entrySet())
+		{
+			context.write(new Text(ent.getKey()+"  "+mnthpg.get(ent.getKey())+"  "),new Text(ent.getValue().toString()));
+		}	
+		
+         context.write(new Text("*********************************"),null);
+		
+		context.write(new Text("\t\t\tHIGHEST VIEWCOUNT IN WHOLE YEAR\t\t\t"),null);
+		context.write(new Text("*********************************"),null);
+		
+	Map.Entry<String, Integer> maxEntry = null;
+
+		for(Entry<String, Integer> entry : pagebucket.entrySet())
+		{
+			if(maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0)
+				maxEntry = entry;
+		}
+		context.write(new Text("Yearly trend"+maxEntry.getKey()),new Text(maxEntry.getValue().toString()));
+    
+		
+		Map.Entry<String, BigInteger> maxEntry1 = null;
+
+		for(Entry<String, BigInteger> entry1 : pagesizelist.entrySet())
+		{
+			if(maxEntry1 == null || entry1.getValue().compareTo(maxEntry1.getValue()) > 0)
+				maxEntry1 = entry1;
+		}
+		 context.write(new Text("*********************************"),null);
+			
+			context.write(new Text("\t\t\tMOST INFORMATIVE PAGE OF THE YEAR\t\t\t"),null);
+			context.write(new Text("*********************************"),null);
+			
+		context.write(new Text("Most Informative Page "+maxEntry1.getKey()),new Text(maxEntry1.getValue().toString()));
+		
+		
+		
+	}//end of cleanup
+	
+
+	
+	
+	}//end of class
